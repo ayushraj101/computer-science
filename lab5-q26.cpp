e46fcc2375df2808f1e6095fb4783796d5b39256
ayushraj101 <42176710+ayushraj101@users.noreply.github.com>
2018-09-03 16:37:26 +0530

    Add files via upload

---
+++ lab5-q26.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<< "Hey" << endl;// hey
+	cout<< "all the odd numbers betn 1 to 100 are"<< endl;//discr
+	int a;//assi int a 
+	a=1;//a=1
+	cout << a << endl;
+	while (a<99){//while a==100
+	a=a+2;//a=a+2
+	cout<<a << endl; //cout a
+	}
+	return 0;
+}
+
